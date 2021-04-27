d459e6a82af4a480269c4288bfcb97e8079aa371
wsadmin <wsadmin@oatk.local>
2021-04-27 19:28:33 +0600

    My first Game

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+const canvas =  document.getElementById("game"); // Получаем елемент поля игры
+const ctx = canvas.getContext("2d"); // Задаём параметры отрисовки
+
+let hero = new Image();
+hero.src = "assets/img/Darwin.png";
+
+let bg = new Image();
+bg.src = "assets/img/Background.png";
+
+function game() {
+    ctx.drawImage(bg, 0, 0, 500, 150);
+    ctx.drawImage(hero, 0, 0, 30, 50);
+
+}
+game();
